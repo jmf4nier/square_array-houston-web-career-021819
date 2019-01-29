da9e0ba33948e4d82250e43b882d694cb475ed09
Jason Fournier <jmf4nier@hotmail.com>
2019-01-29 21:45:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
 array = [1,2,3]
 def square_array(array)
-  new_array = []
-    array.each {|numbers| new_array << numbers** 2 }
+  
+    array.collect {|numbers| num + 1}
+    
       
     
-  new_array
+  
 end
 square_array(array)
